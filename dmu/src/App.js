a0b1372f826168f196ea9bd5473da5520ce64f8a
Kimbosung521 <sorisl37@naver.com>
2026-03-30 21:52:57 +0900

    시작

---
+++ dmu/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+   <div>
+    <div style={{display:'flex', justifyContent: 'center',alignItems:'center',height:"100vh" }}>퀴즈</div>
+   </div>
+  );
+}
+
+export default App;
---
+++ dmu/src/Quzi.js
@@ -0,0 +1,3 @@
+export default function Quzi(){
+    reutnr (<div>아아</div>)
+}
